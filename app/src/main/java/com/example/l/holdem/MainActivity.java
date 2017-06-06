24554bba1c09856b7beaa8d7b34156bb9544aa92
swhdgjss <swhdgjss@naver.com>
2017-06-07 00:49:58 +0900

    06/07

--- app/src/main/java/com/example/l/holdem/MainActivity.java
+++ app/src/main/java/com/example/l/holdem/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.l.holdem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    RadioGroup radioGroup;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, Splash.class));
+
+        button = (Button) findViewById(R.id.button);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name;
+                Intent intent = new Intent();
+
+                if(editText.getText().toString().equals("")) name = "Guest";
+                else name = editText.getText().toString();
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.radioButton:
+                        intent.putExtra("name", name);
+                        break;
+                    case R.id.radioButton2:
+                        intent.putExtra("name", name);
+                        break;
+                    case R.id.radioButton3:
+                        intent.putExtra("name", name);
+                        break;
+                }
+            }
+        });
     }
 }
