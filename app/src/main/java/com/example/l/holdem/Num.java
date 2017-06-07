b6c7e80d0e11b935f1a4b94253143c80887fcd33
swhdgjss <swhdgjss@naver.com>
2017-06-07 16:48:53 +0900

    06/07 숫자를 J,Q,K,A로 바꿈

---
+++ app/src/main/java/com/example/l/holdem/Num.java
@@ -0,0 +1,33 @@
+package com.example.l.holdem;
+
+/**
+ * Created by L on 2017-06-07.
+ */
+
+public class Num {
+    private String[] num = new String[15];
+
+    public Num() {
+        num[0] = "0";
+        num[1] = "0";
+        num[2] = "2";
+        num[3] = "3";
+        num[4] = "4";
+        num[5] = "5";
+        num[6] = "6";
+        num[7] = "7";
+        num[8] = "8";
+        num[9] = "9";
+        num[10] = "10";
+        num[11] = "J";
+        num[12] = "Q";
+        num[13] = "K";
+        num[14] = "A";
+    }
+
+    public String getNum(String num) {
+        int tmp = Integer.parseInt(num);
+
+        return this.num[tmp];
+    }
+}
