a199a30ee046c18e924be1f84817d5fdc7d4dfb9
swhdgjss <swhdgjss@naver.com>
2017-06-07 17:22:17 +0900

    06/07 deck초기화 오류 수정

---
+++ app/src/main/java/com/example/l/holdem/Play6.java
@@ -0,0 +1,13 @@
+package com.example.l.holdem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Play6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play6);
+    }
+}
--- app/src/main/java/com/example/l/holdem/Rule.java
+++ app/src/main/java/com/example/l/holdem/Rule.java
@@ -74,6 +74,7 @@ public class Rule{
 
     public void clear() {
         board.clear();
+        deck.clear();
     }
 
     public String[] hands(ArrayList<Tuple> card) {
